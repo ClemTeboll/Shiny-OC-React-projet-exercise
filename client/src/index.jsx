9992e39e26a23e1eff808c24ba3795059f85215d
Clément Llobet <clement.llobet@tutanota.com>
2022-07-13 11:48:15 +0200

    use Context in components to create dark mode theme

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
 import Error from './components/Errors';
 import Header from './components/Header';
 import Survey from './pages/Survey';
@@ -9,15 +8,8 @@ import Freelances from './pages/Freelances';
 import Home from './pages/Home';
 import Results from './pages/Results';
 import { ThemeProvider } from './utils/context';
-
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-    body {
-      margin: 0;
-    }
-`;
+import GlobalStyle from './utils/style/GlobalStyle';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -32,6 +24,7 @@ root.render(
         <Route path="freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </ThemeProvider>
   </Router>
 );
